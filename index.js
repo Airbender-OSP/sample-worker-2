927d558fc67145f2d72ffe26bbaa392a20fe8b5b
Kristen Althoff <kristenwalthoff@gmail.com>
2022-03-12 16:23:56 -0500

    updated sending to 3000

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // const express = require('express')
 // const WebSocket = require('ws');
+//const websocket = new WebSocket('ws://localhost:8787');
 
-// const socket = new WebSocket('ws://localhost:8787');
+
+
+// import WebSocket from 'ws';
+// const ws = new WebSocket('ws://www.host.com/path');
+
+// ws.on('open', function open(){
+//   ws.send('something');
+// });
+
+// ws.on('message', function message(data){
+//   console.log('received: %s', data);
+// });
+
+
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -15,3 +29,21 @@ async function handleRequest(request) {
     headers: { 'content-type': 'text/plain' },
   })
 }
+
+// export default {
+//   async fetch(request) {
+//     const res = await fetch("https://echo.websocket.org", {
+//       headers: { Upgrade: "websocket" },
+//     });
+
+//     const webSocket = res.webSocket;
+//     webSocket.accept();
+//     webSocket.addEventListener("message", (event) => {
+//       console.log(event.data);
+//     });
+
+//     webSocket.send("Hello!"); // Above listener logs "Hello!"
+
+//     return new Response();
+//   },
+// };
